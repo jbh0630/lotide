d732b1871bf98c0be3026e9609d8c552f97f4b6c
jbh0630 <jayjo0630@gmail.com>
2021-05-24 22:36:52 +0000

    Implement head

---
+++ head.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === undefined) {
+    return "Array is undefined";
+  } else {
+    if (actual === expected) {
+      return `Assertion Passed: ${actual} === ${expected}`;
+    } else {
+      return `Assertion Failed: ${actual} !== ${expected}`;
+    }
+  }
+};
+
+const head = (array) => {
+  const headOfArray = array[0];
+  if (headOfArray === undefined) {
+    return undefined;
+  } else {
+    return headOfArray;
+  }
+}
+
+console.log(assertEqual(head([]), 5));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+console.log(assertEqual(head([5,6,7]), 5));
