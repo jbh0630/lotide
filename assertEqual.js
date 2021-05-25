d72779f85cf32c6872f95b3e74d8f5d58e389d65
jbh0630 <jayjo0630@gmail.com>
2021-05-25 03:00:59 +0000

    modified emojis

--- assertEqual.js
+++ assertEqual.js
@@ -1,10 +1,9 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return `Assertion Passed: ${actual} === ${expected}`;
+    return `✅✅✅Assertion Passed: ${actual} === ${expected}`;
   } else {
-    return `Assertion Failed: ${actual} !== ${expected}`;
+    return `🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`;
   }
 };
 console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
 console.log(assertEqual(1, 1));
-
