69f3780bde6e2e8eeb465d34902172c59f0bb938
jbh0630 <jayjo0630@gmail.com>
2021-05-26 02:58:03 +0000

    Implement without

---
+++ without.js
@@ -0,0 +1,34 @@
+const eqArrays = function(a, b) {
+  let boolean = false;
+  if (a.length === b.length) {
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] === b[i]) {
+        boolean = true;
+      } else {
+        boolean = false;
+      }
+    }
+    return boolean;
+  } else {
+    return boolean;
+  }
+};
+
+const assertArraysEqual = function(bool) {
+ 
+    if (bool === true) {
+      return `✅✅✅ Assertion Passed`;
+    } else {
+      return `🛑🛑🛑 Assertion Failed`;
+    }
+};
+
+const without = function(source, itemsToRemove) {
+   
+  return source.filter(s => !itemsToRemove.includes(s));
+};
+
+const words = ["hello", "world", "lighthouse"];
+console.log(assertArraysEqual(eqArrays(words, ["hello", "world", "lighthouse"])));
+console.log(assertArraysEqual(eqArrays(without(words, ["lighthouse"]), ["hello", "world"])));
+console.log(assertArraysEqual(eqArrays(without(words, ["world"]), ["hello", "world"])));
