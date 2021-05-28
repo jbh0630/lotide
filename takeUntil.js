6dceda8c5e8b6b68cfb76c93b9d76c447f44fc6c
jbh0630 <jayjo0630@gmail.com>
2021-05-28 04:50:57 +0000

    Implement takeUntil modified

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,27 @@
+const eqArrays = function(actual, expected) {
+  let boolean = false;
+  if (actual.length === expected.length) {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] === expected[i]) {
+        boolean = true;
+      } else {
+        boolean = false;
+      }
+    }
+  }
+  return boolean;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  const eqArrayResult = eqArrays(actual, expected);
+  if (eqArrayResult === true) {
+    return `✅✅✅ Assertion Passed: [${actual}] === [${expected}]`;
+  } else {
+    return `🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`;
+  }
+};
+
+
 const takeUntil = function(array, callback) {
 
   const newArray = [];
@@ -12,10 +36,10 @@ const takeUntil = function(array, callback) {
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
+console.log(assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]));
 
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+console.log(assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]));
