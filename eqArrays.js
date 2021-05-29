3fd7126003de45f7464658a93bf3f390f26eeb1a
jbh0630 <jayjo0630@gmail.com>
2021-05-29 07:53:01 +0000

    Recursive eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -7,20 +7,23 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(a, b) {
-  let boolean = false;
-  if (a.length === b.length) {
-    for (let i = 0; i < a.length; i++) {
-      if (a[i] === b[i]) {
-        boolean = true;
-      } else {
-        boolean = false;
-      }
-    }
+
+  if (a.length !== b.length) {
+    return false;
   }
-  return boolean;
+
+  for (let i = 0; i < a.length; i++) {
+    if (Array.isArray(a[i]) && (eqArrays(a[i], b[i]) === false)) {
+      return false;
+    }
+    if (!(Array.isArray(a[i])) && a[i] !== b[i]) { 
+      return false;
+    }
+  } 
+  return true;
 };
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
-console.log(assertEqual(eqArrays([1, 2, 4], [1, 2, 3]), true));
-console.log(assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3, 4]), true));
-console.log(assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), true));
-console.log(assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), false));
+
+
+console.log(assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true));
+console.log(assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false));
+console.log(assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false));
